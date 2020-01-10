cababae5882f1c828be18c907f95af1f3531be4c
ekmorris <47565801+ekmorris@users.noreply.github.com>
2020-01-10 10:24:55 -0500

    made enemies move

--- ICS4UC_RST/src/rst/SpaceDodgers.java
+++ ICS4UC_RST/src/rst/SpaceDodgers.java
@@ -15,7 +15,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import rst.GetToTheOtherSide.GameTimer;
+
 
 public class SpaceDodgers extends Application {
 
@@ -27,7 +27,12 @@ public class SpaceDodgers extends Application {
 	private static final int SOUTH = 4;
 	private static final int EAST = 4;
 	private static final int WEST = -4;
-	private static final int ENEMY_SPEED = 7;
+	
+	private static  int enemy1Speed = 5;
+	private static  int enemy2Speed = 5;
+	private static  int enemy3Speed = 5;
+	private static  int enemy4Speed = 5;
+	private static  int enemy5Speed = 5;
 	private int shipSpeedX = 2;
 	private int shipSpeedY = 0;
 	KeyCode code;
@@ -85,37 +90,37 @@ public class SpaceDodgers extends Application {
 		ship.setX(350);
 
 		URL enemyLocation1 = SpaceDodgers.class.getResource("Enemy.png");
-		enemy1 = new ImageView(enemyLocation1.toString());
-		enemy1.setFitHeight(70);
-		enemy1.setFitWidth(70);
-		enemy1.setX(160);
-		enemy1.setY(0);
 		
+		enemy1 = new ImageView(enemyLocation1.toString());
+		enemy1.setFitHeight(65);
+		enemy1.setFitWidth(65);
+		enemy1.setX(155);
+		enemy1.setY(0);
 		
 		enemy2 = new ImageView(enemyLocation1.toString());
 		enemy2.setFitHeight(65);
 		enemy2.setFitWidth(65);
-		enemy2.setX(320);
+		enemy2.setX(310);
 		enemy2.setY(0);
 		
 		enemy3 = new ImageView(enemyLocation1.toString());
 		enemy3.setFitHeight(65);
 		enemy3.setFitWidth(65);
-		enemy3.setX(480);
+		enemy3.setX(465);
 		enemy3.setY(0);
 		
 		enemy4 = new ImageView(enemyLocation1.toString());
 		enemy4.setFitHeight(65);
 		enemy4.setFitWidth(65);
-		enemy4.setX(640);
+		enemy4.setX(620);
 		enemy4.setY(0);
-		
+	
 		enemy5 = new ImageView(enemyLocation1.toString());
 		enemy5.setFitHeight(65);
 		enemy5.setFitWidth(65);
-		enemy5.setX(0);
+		enemy5.setX(774);
 		enemy5.setY(0);
-	
+		
 		 timer = new GameTimer();
 	        timer.start();
 		
@@ -164,13 +169,35 @@ public class SpaceDodgers extends Application {
 			
 			Bounds shipBounds = ship.getBoundsInLocal();
 			Bounds menuBounds = menu.getBoundsInLocal();
+			
 			Bounds enemy1Bounds = enemy1.getBoundsInLocal();
 			Bounds enemy2Bounds = enemy2.getBoundsInLocal();
 			Bounds enemy3Bounds = enemy3.getBoundsInLocal();
 			Bounds enemy4Bounds = enemy4.getBoundsInLocal();
-			Bounds enemy5Bounds = enemy5.getBoundsInLocal();
+			
 
-
+		
+			if(enemy1.getX() <= 0 || enemy1.getX() >= 775) {
+				enemy1Speed = enemy1Speed*-1;
+			}
+			if(enemy2.getX() <= 0 || enemy2.getX() >= 775) {
+				enemy2Speed = enemy2Speed*-1;
+			}
+			if(enemy3.getX() <= 0|| enemy3.getX() >= 775) {
+				enemy3Speed = enemy3Speed*-1;
+			}
+			if(enemy4.getX() <= 0|| enemy4.getX() >= 775) {
+				enemy4Speed = enemy4Speed*-1;
+			}
+			if(enemy5.getX() <= 0|| enemy5.getX() >= 775) {
+				enemy5Speed = enemy5Speed*-1;
+			}
+			enemy1.setX(enemy1.getX() + enemy1Speed);
+			enemy2.setX(enemy2.getX() + enemy2Speed);
+			enemy3.setX(enemy3.getX() + enemy3Speed);
+			enemy4.setX(enemy4.getX() + enemy4Speed);
+			enemy5.setX(enemy5.getX() + enemy5Speed);
+			
 			if(shipBounds.intersects(menuBounds)  && code == KeyCode.DOWN){
 				shipSpeedX = 0;
 				shipSpeedY = 0;
@@ -187,6 +214,7 @@ public class SpaceDodgers extends Application {
 				shipSpeedX = 0;
 				shipSpeedY = 0;
 			}
+			
 		}
 	}
 }
